f16750092d8009c9888d0d9b7239e54fcc1698b9
Szalapbp <szalapbp@mail.uc.edu>
2025-10-27 01:27:26 -0400

    Project Created and Packages Added

---
+++ Group6_WSAD_FinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group6_WSAD_FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Group6_WSAD_FinalProject
+Final Project for Group 6 Web Server App Dev Fall 2025
