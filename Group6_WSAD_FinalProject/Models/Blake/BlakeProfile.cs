556a5d562955857b3022a7c14c25a33d1de650b7
Szalapbp <szalapbp@mail.uc.edu>
2025-10-28 17:31:36 -0400

    Blake Controller, models, and views added. .gitignore added
    Controller, models, and views for Blake's pages have been added and configured. The database and DB context for blake has not been setup yet. I plan on doing that another time. I also added the .gitignore so that it does not track any temp files.

---
+++ Group6_WSAD_FinalProject/Models/Blake/BlakeProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace Group6_WSAD_FinalProject.Models.Blake
+{
+    public class BlakeProfile
+    {
+        public int Id { get; set; }
+        public string Hobby { get; set; }
+        public string Description { get; set; }
+
+    }
+}
